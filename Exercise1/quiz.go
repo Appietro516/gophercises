6a4c72b47bf46369081b3a3f2ce9b6375a3177da
Anthony <appietro516@email.arizona.edu>
2021-04-12 21:10:14 -0700

    Exercise 1 Part 2

--- Exercise1/quiz.go
+++ Exercise1/quiz.go
@@ -4,42 +4,50 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	score := 0
 	csvFilename := flag.String("csv", "questions.csv", "CSV file")
+	timeLimit := flag.Int("limit", 30, "Time limit in seconds")
 	flag.Parse()
+
 	file, err := os.Open(*csvFilename)
 	if err != nil {
 		fmt.Printf("ERROR")
+		os.Exit(1)
 	}
-	reader := csv.NewReader(file)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
-	//loop over file
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		//write question
-		fmt.Printf("%s\n", strings.Join(record[:len(record)-1], ","))
-		//read answer
-		var answer string
-		fmt.Scanln(&answer)
-		if answer == record[len(record)-1] {
-			score += 1
-		} else {
-			break
-		}
-	}
-	file.Seek(0, 0)
+	reader := csv.NewReader(file)
 	lines, err := reader.ReadAll()
 	if err != nil {
-		fmt.Printf("ERROR")
+		os.Exit(1)
+	}
+
+  	problemLoop:
+	for _, record := range lines {
+		fmt.Printf("%s\n", strings.Join(record[:len(record)-1], ","))
+
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanln(&answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("Time exceeded\n")
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == record[len(record)-1] {
+				score += 1
+			}
+		}
 	}
 
 	fmt.Printf("You scored %v out of %v\n", score, len(lines))
